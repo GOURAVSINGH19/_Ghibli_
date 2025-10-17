b236f090c6d3c36a9bc6ec292a7e7441b4899ea0
GOURAVSINGH19 <gouravsingh4495@gmail.com>
2025-10-17 17:07:20 +0530

    _init_grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import Grid from "../components/Grid";
 import Home from "../components/Home";
 
 const ComponentLibraryDemo = () => {
   return (
-    <div className="w-screen h-screen">
+    <main className="w-screen h-screen bg_svg">
       <div className=" max-w-screen-xl mx-auto p-[.8rem] px-[2rem] h-full">
         <Home/>
+        <Grid/>
       </div>
-    </div>
+    </main>
   );
 };
 
--- components/Home.tsx
+++ components/Home.tsx
@@ -1,20 +1,16 @@
 import { ArrowRight } from 'lucide-react'
 import React from 'react'
-import Button from './microInteraction/Button'
 
 const Home = () => {
     return (
-        <div className='w-full h-full flex items-center justify-center flex-col'>
-            <h1 className='text-[8vw] line-clamp-2 leading-[80%] capitalize'>Build beautiful, consistent UIs — fast.</h1>
-            <div className=' mt-10 backdrop-blur-2xl  w-[30%] p-1 rounded-2xl flex items-center justify-center gap-3 mb-5'>
+        <section className='w-full h-1/2 flex items-center mt-[4vw] flex-col mb-10'>
+            <div className='mt-10 backdrop-blur-2xl bg-white/10 border border-white/10 w-[30%] px-4 py-1/2 rounded-2xl flex items-center justify-center gap-3 mb-5 shadow-[var(--shadow-sm)]'>
                 <div className='w-2 h-2 rounded-full bg-purple-800'></div>
-                <p className='text-[11px]'>A modern UI library designed for speed, flexibility, and simplicity.</p>
+                <p className='text-[10px]'>A modern UI library designed for speed, flexibility, and simplicity.</p>
                 <ArrowRight className='w-3 font-light' />
             </div>
-            <div className='button relative'>
-                <Button text="Get Started"  />
-            </div>
-        </div>
+            <h1 className='text-[8vw] line-clamp-2 leading-[110%] capitalize'>Build beautiful, consistent UIs — fast.</h1>
+        </section>
     )
 }
 
